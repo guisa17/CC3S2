2f223200d6b882173a1343f8afd5e61b24ffdfc5
Guillermo Ronie Salcedo Alvarez <139187191+guisa17@users.noreply.github.com>
2023-09-04 17:37:14 -0500

    Update platforme.cpp

--- PruebaEntrada-CC3S2/platforme.cpp
+++ PruebaEntrada-CC3S2/platforme.cpp
@@ -4,25 +4,49 @@ using namespace std;
 
 typedef vector<pair<int, pair<int, int>>> coords;
 
-
-void solve(int n, const coords &pltf)
+void solve(int n, coords &p)
 {
-    int ans = 0;
+    sort(p.begin(), p.end());
 
-    for (const auto& p : pltf)
+    int ans = 2 * p[0].first;
+
+    for (int i = 1; i < n; i++)
     {
-        int y = p.first;
-        int x1 = p.second.first;
-        int x2 = p.second.second;
+        int y = p[i].first;
+        int x1 = p[i].second.first;
+        int x2 = p[i].second.second;
 
-        // TODO
+        bool flag1 = false, flag2 = false;
+
+        for (int j = i - 1; j >= 0; j--)
+        {
+            int y_ = p[j].first;
+            int x1_ = p[j].second.first;
+            int x2_ = p[j].second.second;
+
+            if (!flag1 && x1 > x1_ && x1 < x2_)
+            {
+                ans += y - y_;
+                flag1 = true;
+            }
+            if (!flag2 && x2 > x1_ && x2 < x2_)
+            {
+                ans += y - y_;
+                flag2 = true;
+            }
+        }
+        if (!flag1) ans += y;
+        if (!flag2) ans += y;
     }
-
+    cout << ans;
 }
 
 
 int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int n;
     cin >> n;
     coords platforms;
@@ -33,7 +57,6 @@ int main()
         cin >> y >> x1 >> x2;
         platforms.push_back({y, {x1, x2}});
     }
-
     solve(n, platforms);
 
     return 0;
