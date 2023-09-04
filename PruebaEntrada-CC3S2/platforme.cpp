a0ef47dc1a3cd4c372a50500a74fcff9c70549b1
Guillermo Ronie Salcedo Alvarez <139187191+guisa17@users.noreply.github.com>
2023-09-04 12:57:37 -0500

    Add files via upload

---
+++ PruebaEntrada-CC3S2/platforme.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+void solve()
+{
+
+}
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[3][n];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++) cin >> arr[j][i];
+    }
+
+    solve();
+
+    return 0;
+}
